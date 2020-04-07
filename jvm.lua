4fe4367c862270360c9955523fbda5acc168eb3f
danilacasito <54474838+danilacasito@users.noreply.github.com>
2020-04-07 20:37:30 +0200

    Create jvm.lua

---
+++ jvm.lua
@@ -0,0 +1,13 @@
+while true do
+  shell.run("clear")
+  paintutils.drawLine(1, 1, 8, 1,colors.green)
+  term.setCursorPos(1, 1)
+  print("X Start")
+  local event, button, x, y = os.pullEvent()
+  if y == 1 then
+    if x == 1 then
+      shell.run("clear")
+      write("Execute Command: ")
+    end
+  end
+end
